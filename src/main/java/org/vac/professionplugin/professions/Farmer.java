a9d35ff3875e7af358fec63acc0092a823d49161
Cristian Martínez <dkavik.eldk@gmail.com>
2023-08-05 15:36:42 -0400

    Refactor Farmer class and add event handlers
    - Refactored the Farmer class to extend the Profession class
    - Added event handler methods for PlayerMoveEvent, BlockBreakEvent, EntityDeathEvent, EntityDamageByEntityEvent, EntityDamageEvent, EntityShootBowEvent, EntityExplodeEvent, and EntityBreedEvent
    - Implemented empty reward methods for levels 5, 10, 15, and 20
    - Added a leaveProfession method

--- src/main/java/org/vac/professionplugin/professions/Farmer.java
+++ src/main/java/org/vac/professionplugin/professions/Farmer.java
@@ -1,5 +1,99 @@
 package org.vac.professionplugin.professions;
 
-public class Farmer
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.*;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.Inventory;
+
+public class Farmer extends Profession
 {
+    public Farmer(int level, float exp, Player player)
+    {
+        super("Granjero", level, exp, player);
+    }
+
+    @Override
+    public Inventory getInventoryProfessionData()
+    {
+        return null;
+    }
+
+    @Override
+    public void onPlayerMove(PlayerMoveEvent event)
+    {
+
+    }
+
+    @Override
+    public void onBlockBreak(BlockBreakEvent event)
+    {
+
+    }
+
+    @Override
+    public void onEntityDeath(EntityDeathEvent event)
+    {
+
+    }
+
+    @Override
+    public void onEntityDamage(EntityDamageByEntityEvent event)
+    {
+
+    }
+
+    @Override
+    public void onEntityDamage(EntityDamageEvent event)
+    {
+
+    }
+
+    @Override
+    public void onPlayerShootBow(EntityShootBowEvent event)
+    {
+
+    }
+
+    @Override
+    public void onEntityExplode(EntityExplodeEvent event)
+    {
+
+    }
+
+    @Override
+    public void onEntityBreed(EntityBreedEvent event)
+    {
+
+    }
+
+    @Override
+    public void level5Reward()
+    {
+
+    }
+
+    @Override
+    public void level10Reward()
+    {
+
+    }
+
+    @Override
+    public void level15Reward()
+    {
+
+    }
+
+    @Override
+    public void level20Reward()
+    {
+
+    }
+
+    @Override
+    public void leaveProfession()
+    {
+
+    }
 }
